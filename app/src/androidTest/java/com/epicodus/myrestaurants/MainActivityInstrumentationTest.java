041b7b91f4afc3ca2533b61788f7167b4d2c1722
Eric Krause <krause197@gmail.com>
2016-12-05 16:30:11 -0800

    daily homework revisions

--- app/src/androidTest/java/com/epicodus/myrestaurants/MainActivityInstrumentationTest.java
+++ app/src/androidTest/java/com/epicodus/myrestaurants/MainActivityInstrumentationTest.java
@@ -20,19 +20,19 @@ public class MainActivityInstrumentationTest {
     public ActivityTestRule<MainActivity> activityTestRule =
             new ActivityTestRule<>(MainActivity.class);
 
-    @Test
-    public void validateEditText() {
-        onView(withId(R.id.locationEditText)).perform(typeText("Portland"))
-                .check(matches(withText("Portland")));
-    }
-
-    @Test
-    public void locationIsSentToRestaurantsActivity() {
-        String location = "Portland";
-        onView(withId(R.id.locationEditText)).perform(typeText(location));
-        onView(withId(R.id.findRestaurantsButton)).perform(click());
-        onView(withId(R.id.locationTextView)).check(matches
-                (withText("Here are all the restaurants near: " + location)));
-    }
+//    @Test
+//    public void validateEditText() {
+//        onView(withId(R.id.locationEditText)).perform(typeText("Portland"))
+//                .check(matches(withText("Portland")));
+//    }
+//
+//    @Test
+//    public void locationIsSentToRestaurantsActivity() {
+//        String location = "Portland";
+//        onView(withId(R.id.locationEditText)).perform(typeText(location));
+//        onView(withId(R.id.findRestaurantsButton)).perform(click());
+//        onView(withId(R.id.locationTextView)).check(matches
+//                (withText("Here are all the restaurants near: " + location)));
+//    }
 
 }
--- app/src/main/java/com/epicodus/myrestaurants/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/ui/MainActivity.java
@@ -1,23 +1,36 @@
 package com.epicodus.myrestaurants.ui;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Typeface;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.epicodus.myrestaurants.Constants;
 import com.epicodus.myrestaurants.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     @Bind(R.id.findRestaurantsButton) Button mFindRestaurantsButton;
-    @Bind(R.id.locationEditText) EditText mLocationEditText;
     @Bind(R.id.appNameTextView) TextView mAppNameTextView;
+    @Bind(R.id.savedRestaurantsButton) Button mSavedRestaurantsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,15 +42,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mAppNameTextView.setTypeface(ostrichFont);
 
         mFindRestaurantsButton.setOnClickListener(this);
+        mSavedRestaurantsButton.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         if(v == mFindRestaurantsButton) {
-            String location = mLocationEditText.getText().toString();
             Intent intent = new Intent(MainActivity.this, RestaurantListActivity.class);
-            intent.putExtra("location", location);
             startActivity(intent);
         }
+
+        if (v == mSavedRestaurantsButton) {
+            Intent intent = new Intent(MainActivity.this, SavedRestaurantListActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_logout) {
+            logout();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
     }
 }
