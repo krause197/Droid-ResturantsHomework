acca0530696331f50f22396e3658c59bab3e9a38
Eric Krause <krause197@gmail.com>
2016-12-12 09:57:11 -0800

    add monday homework to restaurant app for reording lists and animations

--- app/src/main/java/com/epicodus/myrestaurants/models/Restaurant.java
+++ app/src/main/java/com/epicodus/myrestaurants/models/Restaurant.java
@@ -16,6 +16,7 @@ public class Restaurant {
     double latitude;
     double longitude;
     List<String> categories = new ArrayList<>();
+    String index;
 
     // empty constructor needed by the Parceler library:
     public Restaurant() {}
@@ -32,6 +33,7 @@ public class Restaurant {
         this.latitude = latitude;
         this.longitude = longitude;
         this.categories = categories;
+        this.index = "not_specified";
     }
 
     public String getName() {
@@ -71,5 +73,13 @@ public class Restaurant {
     public String getLargeImageUrl(String imageUrl) {
         String largeImageUrl = imageUrl.substring(0, imageUrl.length() - 6).concat("o.jpg");
         return largeImageUrl;
+    }
+
+    public String getIndex() {
+        return index;
+    }
+
+    public void setIndex(String index) {
+        this.index = index;
     }
 }
---
+++ app/src/main/java/com/epicodus/myrestaurants/util/ItemTouchHelperViewHolder.java
@@ -0,0 +1,9 @@
+package com.epicodus.myrestaurants.util;
+
+/**
+ * Created by Guest on 12/12/16.
+ */
+public interface ItemTouchHelperViewHolder {
+    void onItemSelected();
+    void onItemClear();
+}
